29c3ee9525c40e773e2e141be672f6962b23af5e
Aswin Kumar S <119483113+Aswin-Kumars@users.noreply.github.com>
2023-10-13 18:21:43 +0530

    Add files via upload

---
+++ hospital/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Hospital Management System',
+    'summary': '',
+    'version': '16.0.1.0',
+    'author': 'Aswin',
+    'website': 'https://code-ox.com',
+    'license': 'LGPL-3',
+    'depends': [
+        
+    ],
+    'data':[
+        
+    ]
+}
